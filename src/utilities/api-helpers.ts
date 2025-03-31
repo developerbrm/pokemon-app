90180d97d0661c23b2107b87af6d7bd1a2e21486
developerbrm <developerbrm@gmail.com>
2025-03-31 17:09:57 +0530

    - utilities added

---
+++ src/utilities/api-helpers.ts
@@ -0,0 +1,4 @@
+export const LIST_LIMIT = 20
+
+export const GET_SINGLE_POKEMON = 'https://pokeapi.co/api/v2/pokemon/{id}'
+export const GET_POKEMON_LIST = `https://pokeapi.co/api/v2/pokemon?limit=${LIST_LIMIT}`
