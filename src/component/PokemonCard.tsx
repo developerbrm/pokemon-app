49e9e4656efc676a531d90548987561bd197e3e9
developerbrm <developerbrm@gmail.com>
2025-04-01 10:34:14 +0530

    - search filter added

--- src/component/PokemonCard.tsx
+++ src/component/PokemonCard.tsx
@@ -5,14 +5,17 @@ import RenderOtherCardInfo from './RenderOtherCardInfo'
 
 interface PokemonCard {
   pokemon: PokemonListResponseItem
+  searchKeyword: string
 }
 
 const PokemonCard = (props: PokemonCard) => {
-  const { pokemon } = props
+  const { pokemon, searchKeyword } = props
+
+  if (!pokemon.name.includes(searchKeyword)) return null
 
   return (
     <NavLink to={getDetailsPageRoute(pokemon.name)}>
-      <div className="grid h-fit grid-flow-col justify-between rounded-md bg-slate-200 p-5 shadow-sm">
+      <div className="grid h-[200px] grid-flow-col justify-between rounded-md bg-slate-200 p-5 shadow-sm">
         <h3 className="text-2xl font-bold text-slate-900 capitalize">
           {pokemon?.name}
         </h3>
