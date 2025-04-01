c1d4addb2db1dfbd9e77e82471c380f3ac7d1bc8
developerbrm <developerbrm@gmail.com>
2025-04-01 10:51:28 +0530

    - scroll issue fixed - the bg added for the image

--- src/component/Heading.tsx
+++ src/component/Heading.tsx
@@ -1,5 +1,5 @@
 const Heading = ({ text }: { text: string }) => (
-  <h2 className="my-10 text-center text-4xl font-bold text-slate-700 capitalize">
+  <h2 className="my-5 text-center text-4xl font-bold text-slate-700 capitalize lg:my-10">
     {text}
   </h2>
 )
