88dd81d35e362732d3e0bd81701bbb1ccdef0fcb
Braham Prakash <developerbrm@gmail.com>
2025-07-24 21:19:54 +0530

    - redux removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
-import { Provider } from 'react-redux'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './css/index.css'
 import RoutesProvider from './Routes/RoutesProvider'
-import { store } from './redux/store'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
 
 function App() {
   return (
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <RoutesProvider />
-      </QueryClientProvider>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <RoutesProvider />
+    </QueryClientProvider>
   )
 }
 
